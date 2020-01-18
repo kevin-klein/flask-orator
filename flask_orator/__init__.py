85a642036299e8d9d7174ccbe371548a6edd5b2d
kevin <kevin.k1252@gmail.com>
2020-01-18 19:48:21 +0100

    orator fix

--- flask_orator/__init__.py
+++ flask_orator/__init__.py
@@ -51,17 +51,7 @@ class Orator(object):
         return int(request.args.get('page', 1))
 
     def init_commands(self):
-        self.cli = Application(
-            orator_application.get_name(),
-            orator_application.get_version(),
-            complete=True
-        )
-
-        for command in orator_application.all().values():
-            if isinstance(command, Command):
-                self.cli.add(command.__class__(self._db))
-            else:
-                self.cli.add(command)
+        self.cli = orator_application
 
     def register_handlers(self, app):
         self._register_error_handlers(app)
